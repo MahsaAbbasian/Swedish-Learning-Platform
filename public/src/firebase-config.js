a763dd9fd0c8985a04236bc8030f785389b466db
Mahsa Abbasian <abbasian.mahsa@gmail.com>
2024-05-23 01:22:14 +0200

    Remove nested repository and add public directory

---
+++ public/src/firebase-config.js
@@ -0,0 +1,15 @@
+// firebase-config.js
+import { initializeApp } from 'firebase/app';
+
+const firebaseConfig = {
+  apiKey: "API_KEY",
+  authDomain: "PROJECT_ID.firebaseapp.com",
+  projectId: "PROJECT_ID",
+  storageBucket: "PROJECT_ID.appspot.com",
+  messagingSenderId: "SENDER_ID",
+  appId: "APP_ID"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export default app;
